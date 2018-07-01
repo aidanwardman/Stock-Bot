c8f72598d71adae7a4d98e9a330b2322d1096e1e
Metafor <33998784+aidanwardman@users.noreply.github.com>
2018-07-01 17:50:33 +1000

    Initial Setup

---
+++ bot.js
@@ -0,0 +1,19 @@
+var Discord = require('discord.io');
+
+var bot = new Discord.Client({
+    token: "",
+    autorun: true
+});
+
+bot.on('ready', function() {
+    console.log('Logged in as %s - %s\n', bot.username, bot.id);
+});
+
+bot.on('message', function(user, userID, channelID, message, event) {
+    if (message === "ping") {
+        bot.sendMessage({
+            to: channelID,
+            message: "pong"
+        });
+    }
+});
