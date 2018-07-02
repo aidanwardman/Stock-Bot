eaa8d455a65e3f929bdec2ab03dec8b22ea204c5
Metafor <33998784+aidanwardman@users.noreply.github.com>
2018-07-02 11:22:33 +1000

    updates

--- bot.js
+++ bot.js
@@ -16,30 +16,22 @@ bot.on('ready', function() {
 bot.on('message', function(user, userID, channelID, message, event) {
 	var rxp = new RegExp(/^(asx:)[a-z0-9]{3}/);
 	if (rxp.test(message)){
-		var market = message.substr(0,3);
+		var market = message.substr(0,3).toUpperCase();
 		var code = message.substr(4,3).toUpperCase();
 		var url = "https://finance.google.com/finance/getprices?p=1d&i=60&x=ASX&f=c&q="+code;
 		console.log(url);
-		scrape(url,channelID);
-		/*bot.sendMessage({
-            to: channelID,
-            message: "Extracting company code ("+code+") from stock market ("+market+")"
-        });*/
+		scrape(url,channelID,code,market);
 	}
 });
 
-function scrape(url,channelID){
+function scrape(url,channelID,code,market){
     request(url, function(error, response, html){
 		console.log('error:', error); // Print the error if one occurred
 		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-		console.log(html);
         if(!error){
-            //var $ = cheerio.load(html);
-			//var price = extract('<span ng-show="share.last_price" class="ng-binding">','</span>',html);
             var rows = html.split(/\r?\n/);
-			var price = rows[rows.length - 2];
-			//var price = $('.watchlist-last').html();
-			console.log("Price: $"+price);
+			var price = code+": $"+rows[rows.length - 2];
+			console.log("Price: "+price);
 			bot.sendMessage({
 				to: channelID,
 				message: price
@@ -52,34 +44,3 @@ function scrape(url,channelID){
 		}
 	});
 }
-
-function extract(prefix, suffix, s){
-	console.log("Running Extract",prefix,suffix);
-	var i = s.indexOf(prefix);
-	console.log("index 1: "+i);
-	if (i >= 0) {
-		s = s.substring(i + prefix.length);
-	}
-	else {
-		return '';
-	}
-	if (suffix) {
-		i = s.indexOf(suffix);
-		console.log("index 2: "+i);
-		if (i >= 0) {
-			s = s.substring(0, i);
-		}
-		else {
-		  return '';
-		}
-	}
-	console.log(s);
-	return s;
-}
-
-
-//https://www.asx.com.au/asx/share-price-research/company/A2M
-
-//<span ng-show="share.last_price" class="ng-binding">10.520</span>
-
-//<small ng-switch-default="" class="price-date ng-binding ng-scope">29 Jun 2018</small>
